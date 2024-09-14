cc854771a6446256e20724c69905f0d2598f9673
Mohamed323-sudo <saciidm088@gmail.com>
2024-09-14 11:38:25 +0600

    Frontend/Build and style the navigation panel

---
+++ script.js
@@ -0,0 +1,24 @@
+let sideNav = document.getElementById("sideNav");
+let closeIcon = document.getElementById("closeIcon");
+let menuIcon = document.getElementById("menuIcon");
+let menuBtn = document.getElementById("menuBtn");
+let body = document.querySelector("body");
+
+sideNav.style.left = "-200px";
+menuIcon.addEventListener("click", function(){
+    if(sideNav.style.left === "-200px"){
+        sideNav.style.left = "0";
+        menuBtn.style.left = "150px";
+        body.style.overflowY = "hidden";
+        
+        
+    }else{
+        sideNav.style.left = "-200px";
+        menuBtn.style.left = "-150px";
+        body.style.overflowY= "scroll";
+    }
+})
+closeIcon.addEventListener("click", function(){
+    sideNav.style.left = "-200px";
+    menuBtn.style.left = "-150px";
+})
